be9aa302e9e92b7e4ad154040e758a16c8f1aa0f
Hilded Abdi <hilded-abdi@hotmail.com>
2023-07-11 20:36:26 -0400

    Created a computer quiz game

---
+++ Computer Quiz Game.py
@@ -0,0 +1,42 @@
+print("welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! let's play :) ")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("That is incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("That is incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("That is incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply unit":
+    print("Correct!")
+    score += 1
+else:
+    print("That is incorrect!")
+
+
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 4) * 100) + "%.")
